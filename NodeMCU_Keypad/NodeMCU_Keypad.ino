91fcf3a0857894b344b80f7beb5f25be4bb83475
mmmnic <38957141+mmmnic@users.noreply.github.com>
2019-04-22 18:00:03 +0700

    Add files via upload

--- NodeMCU_Keypad/NodeMCU_Keypad.ino
+++ NodeMCU_Keypad/NodeMCU_Keypad.ino
@@ -4,47 +4,56 @@
 #include <WiFiClient.h>
 #include <Keypad.h>
 
+// Khai báo số của máy
 String MACHINE_ID    = "1";
 String MACHINE_INDEX = "1";
 
 // Nhap ten wifi va mat khau wifi o day
-const char *ssid = "TP-LINK_F608";
-const char *password = "quenpassroi";
-const char *host = "192.168.1.4";
-const int httpPort = 8080;
+const char *ssid = "UIT_Guest";
+const char *password = "1denmuoi1";
+// Nhap dia chi IP cua server va port
+const char *host = "192.168.0.130";
+const int httpPort = 80;
 
+// Khởi tạo client
 WiFiClient client;
 
-const byte n_rows = 4;
+// khai bao số lượng hàng và cột của keypad
+const byte n_rows = 4; 
 const byte n_cols = 4;
- 
+
+// định nghĩa vị trí của các nút bấm
 char keys[n_rows][n_cols] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
- 
-byte colPins[n_rows] = {D3, D2, D1, D0}; //Cách nối chân với nodemcu
-byte rowPins[n_cols] = {D7, D6, D5, D4};
 
-//cài đặt thư viện keypad
+//Cách nối chân với NodeMCU
+byte colPins[n_rows] = {D3, D2, D1, D0};  //cột
+byte rowPins[n_cols] = {D7, D6, D5, D4};  //hàng
+
+//cài đặtkeypad
 Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, n_rows, n_cols); 
 
-
 void setup() {
-  Serial.begin(115200);//bật serial, baudrate 9600
-
-  Serial.println("hello");
+  //bật serial, baudrate 115200
+  Serial.begin(115200);
+  
   // Ket noi wifi
   connectWifi(); 
 }
 
 void loop() {  
+  // tạo biến myKey để lưu giá trị nút được bấm
   char myKey = myKeypad.getKey();
- 
+
+  // kiểm tra nếu có nút bấm
   if (myKey != NULL){
+    // sẽ gửi dữ liệu lên server
     postServer(String (myKey));
+    // hiện nút vừa bấm ở cổng serial
     Serial.println(myKey);
   }
 }
@@ -79,18 +88,14 @@ void postServer(String ERROR_DATA)
   }
   
   // địa chỉ trang cần post
-  String uri = "http://192.168.0.119:8080/Error_Notification_IOT/ErrorNoti.php";
-  
-  // chuỗi data sau khi đã lấy ra từ json
-  String data = "MACHINE_ID=" + MACHINE_ID + "&MACHINE_INDEX=" + MACHINE_INDEX + "&ERROR_INDEX=" + ERROR_DATA;
-  // gửi request post tới server (dưới đây là 1 chuỗi quy tắc post):
-  client.println("POST " + uri + " HTTP/1.0");
-  client.println("Host: " + String(host));
-  client.println("Accept: /");
-  client.print("Content-Length: ");
-  client.println(data.length());
-  client.println("Content-Type: application/x-www-form-urlencoded");
+  String uri = "http://192.168.0.130:80/";
+  // chuỗi data theo một quy định
+  String data = "GET /?" "MACHINE_ID=" + MACHINE_ID + "&MACHINE_INDEX=" + MACHINE_INDEX + "&ERROR_INDEX=" + ERROR_DATA + " HTTP/1.0";
+  // gửi request GET tới server (dưới đây là 1 chuỗi quy tắc GET):
+  client.println(data);
   client.println();
-  client.print(data);
+  // hiện chuỗi request và kết quả ở cổng serial
+  Serial.print("Requesting GET: ");
+  Serial.println(data);
   Serial.println("Gui thanh cong");
 }
